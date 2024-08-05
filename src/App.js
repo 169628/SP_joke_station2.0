ab0b70139c6f8a1a7fb86df43e965aaf3e8aad51
Rainbow Lai <rainbow.pipi.1986@outlook.com>
2024-08-05 14:59:15 +0800

    dev/week1 init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div class="container">
+        <div class="row">
+          <div class="col">Column</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
